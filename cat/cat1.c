1223bcc64b02f25e3824501af5d6344b97194822
jinchangz <jinchangz1@gmail.com>
2024-11-26 09:27:25 +0900

    create head command

--- cat/cat1.c
+++ cat/cat1.c
@@ -1,23 +1,20 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static void do_cat(const char *path);
 static void die(const char *s);
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int i;
-  if (argc < 2)
-  {
+  if (argc < 2) {
     fprintf(stderr, "%s: file name not given\n", argv[0]);
     exit(1);
   }
-  for (i = 1; i < argc; i++)
-  {
+  for (i = 1; i < argc; i++) {
     do_cat(argv[i]);
   }
 
@@ -25,30 +22,22 @@ int main(int argc, char *argv[])
 }
 
 #define BUFFER_SIZE 2048
-static void do_cat(const char *path)
-{
+static void do_cat(const char *path) {
   int fd;
   unsigned char buf[BUFFER_SIZE];
   int n;
   fd = open(path, O_RDONLY);
-  if (fd < 0)
-    die(path);
-  for (;;)
-  {
+  if (fd < 0) die(path);
+  for (;;) {
     n = read(fd, buf, sizeof buf);
-    if (n < 0)
-      die(path);
-    if (n == 0)
-      break;
-    if (write(STDOUT_FILENO, buf, n) < 0)
-      die(path);
+    if (n < 0) die(path);
+    if (n == 0) break;
+    if (write(STDOUT_FILENO, buf, n) < 0) die(path);
   }
-  if (close(fd) < 0)
-    die(path);
+  if (close(fd) < 0) die(path);
 }
 
-static void die(const char *s)
-{
+static void die(const char *s) {
   perror(s);
   exit(1);
 }
